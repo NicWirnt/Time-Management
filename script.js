5298b82285ee4cf3caf7ee247e64246ccb5e455e
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2022-02-15 10:52:25 +1100

    second finish

--- script.js
+++ script.js
@@ -8,16 +8,22 @@ const handleOnSubmit = (e) => {
 
     const task = frmData.get("task");
     const hr = +frmData.get("hr");
+    if(hr<1){
+        alert("Please enter the amount of hour");
+        return;
+    }
 
     const obj = {
         task,
         hr,
     }
-    const ttl = totalTaskHours();
-    if((ttl+hr) >=hrPerWeek){
+    // const ttl = totalTaskHours();
+
+    if((totalTaskHours()+hr) > hrPerWeek){
         alert("Maximum hours reached in a week");
         return;
-    }else{
+    }
+    else{
     taskList.push(obj);
     }
     
@@ -50,14 +56,43 @@ const display = () =>{
 
 }
 
+const displayBad = () =>{
+    let str="";
+    badList.map((item, i) => {
+        str += `
+        <tr>
+        <td>
+            <input type="checkbox" name="" id="">
+            ${item.task}
+        </td>
+        <td>${item.hr}hr</td>
+        <td class="text-end">
+             <button class="btn btn-sm btn-warning" onClick="moveBackItem(${i})"><i class="fa-solid fa-arrow-left"></i></button>
+            <button class="btn btn-sm btn-danger" onClick="deleteBadItem(${i})"><i  class="fas fa-trash-alt" title ="Delete"></i></button>
+            
+        </td>
+        </tr>
+        `
+        
+    })
+    
+    document.getElementById("bad-list").innerHTML = str;
+}
+
 
 const deleteItem = (i) =>{
+    if(!confirm("Are you sure you want to delete this item?")){
+        return;
+    }
     taskList.splice(i,1);
     display();
-    totalTaskHours();
+    
 };
 
 const deleteBadItem = (i) =>{
+    if(!confirm("Are you sure you want to delete this item?")){
+        return;
+    }
     badList.splice(i,1);
     displayBad();
     totalTaskHours();
@@ -84,34 +119,9 @@ const moveBackItem = (i) =>{
     taskList.push(badList[i]);
     badList.splice(i,1);
     display();
-    totalSavedHours();
     displayBad();
+    totalSavedHours();
 }
-
-const displayBad = () =>{
-    let str="";
-    badList.map((item, i) => {
-        str += `
-        <tr>
-        <td>
-            <input type="checkbox" name="" id="">
-            ${item.task}
-        </td>
-        <td>${item.hr}hr</td>
-        <td class="text-end">
-            <button class="btn btn-sm btn-danger" onClick="deleteBadItem(${i})"><i  class="fas fa-trash-alt" title ="Delete"></i></button>
-            <button class="btn btn-sm btn-warning" onClick="moveBackItem(${i})"><i class="fas fa-long-arrow-right" title="Mark as Bad List"></i></button>
-        </td>
-        </tr>
-        `
-        
-    })
-    
-    document.getElementById("bad-list").innerHTML = str;
-}
-
-
-
 
 
 const totalSavedHours = () => {
